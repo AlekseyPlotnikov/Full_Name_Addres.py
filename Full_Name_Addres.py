6eba9999de7a2f33d4a3410f916cc8f562498703
Алексей <Deep1985@yandex.ru>
2021-08-27 02:01:54 +0300

    init

---
+++ Full_Name_Addres.py
@@ -0,0 +1,5 @@
+UserLastName = input('Здравствуйте, введите свою фамилию ')
+UserFistName = input('Введи своё имя ')
+UserPatronymic = input('Введи своё отчество ')
+UserAddress = input('Введите свой адрес ')
+print(f'Здравствуйте, {UserLastName.capitalize()} {UserFistName.capitalize()} {UserPatronymic.capitalize()}.\nВаш заказ был отправлене по адресу {UserAddress.capitalize()}, согласно с полученными данными.')
